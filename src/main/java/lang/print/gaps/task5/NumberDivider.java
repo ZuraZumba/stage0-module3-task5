a281016831e53c2b17375548d9ed1ba7a2783915
zurazumba <zura.zumbadze@yahoo.com>
2023-06-16 00:44:27 +0400

    5-1

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -1,6 +1,13 @@
 package lang.print.gaps.task5;
 
 public class NumberDivider {
-    void divide(int toBeDivided) {
+    void divide(double toBeDivided) {
+        double result = toBeDivided/5;
+        System.out.println(result);
+    }
+    public static void main(String args[]){
+        NumberDivider divider = new NumberDivider();
+        double number = 19;
+        divider.divide(number);
     }
 }
